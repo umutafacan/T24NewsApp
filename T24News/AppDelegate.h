374be171ef2522ef1674a9c9f032d11475915039
Umut Afacan <umut-afacan@hotmail.com>
2016-07-11 19:38:00 +0300

    Initial Commit

---
+++ T24News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  T24News
+//
+//  Created by umut on 7/11/16.
+//  Copyright © 2016 Umut Afacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
